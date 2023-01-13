e9c119fe505363e98d809901a5912bdcc613c50b
mark-summerfield <mark@qtrac.eu>
2023-01-13 07:31:53 +0000

    improved & extended

--- accelhints.go
+++ accelhints.go
@@ -25,34 +25,66 @@ const (
 	maxWeight = 90000.0
 )
 
-// Updates items inserting '&'s to indicate accelerators. Only characters in
-// the Alphabet are candidates. Returns the number of accelerated items.
-func AddHints(items []string) (int, error) {
-	return AddHintsFull(items, Marker, Alphabet)
+// Returns items with '&'s to indicate accelerators. Only characters in
+// the Alphabet are candidates. Use '&&' for literal '&'s.
+func Hints(items []string) ([]string, error) {
+	return HintsFull(items, Marker, Alphabet)
 }
 
-// Updates items inserting marker's (only ASCII allowed) to indicate
+// Returns items with marker's (only ASCII allowed) to indicate
 // accelerators with characters from the given alphabet (of unique uppercase
-// characters) as candidates. Returns the number of accelerated items.
-func AddHintsFull(items []string, marker byte, alphabet string) (int, error) {
-	normalizeMarker(items, marker)
+// characters) as candidates. Use marker + marker for literal markers.
+func HintsFull(items []string, marker byte, alphabet string) ([]string,
+	error) {
+	lines := normalized(items, marker)
 	chars := []rune(alphabet)
-	weights := getWeights(items, marker, chars)
+	weights := getWeights(lines, marker, chars)
 	m, err := munkres.NewHungarianAlgorithm(weights)
 	if err != nil {
-		return 0, err
+		return nil, err
 	}
 	indexes := m.Execute()
-	done := updateItems(items, marker, chars, indexes)
-	return done, nil
+	lines = applyIndexes(items, marker, chars, indexes)
+	return lines, nil
 }
 
-func normalizeMarker(items []string, marker byte) {
+// Returns a list of accelerator indexes in the given hinted strings, and
+// how many have indexes (i.e., a count of those with an index > -1).
+// For those with no accelerator their index value is -1.
+// Assumes the marker is '&'.
+func Indexes(hinted []string) ([]int, int) {
+	return IndexesFull(hinted, '&')
+}
+
+// Returns a list of accelerator indexes in the given hinted strings, and
+// how many have indexes (i.e., a count of those with an index > -1).
+// For those with no accelerator their index value is -1.
+func IndexesFull(hinted []string, marker byte) ([]int, int) {
+	indexes := make([]int, 0, len(hinted))
+	m := string(marker)
+	mm := m + m
+	count := 0
+	for _, hint := range hinted {
+		hint = strings.ReplaceAll(hint, mm, "||")
+		i := strings.IndexByte(hint, marker)
+		indexes = append(indexes, i)
+		if i > -1 {
+			count++
+		}
+	}
+	return indexes, count
+}
+
+func normalized(items []string, marker byte) []string {
+	lines := make([]string, 0, len(items))
 	rx := regexp.MustCompile(`[_&]`)
 	m := string(marker)
-	for i := 0; i < len(items); i++ {
-		items[i] = rx.ReplaceAllLiteralString(items[i], m)
+	mm := m + m
+	for _, line := range items {
+		lines = append(lines, rx.ReplaceAllLiteralString(
+			strings.ReplaceAll(line, mm, "||"), m))
 	}
+	return lines
 }
 
 func getWeights(items []string, marker byte, chars []rune) weights {
@@ -62,9 +94,9 @@ func getWeights(items []string, marker byte, chars []rune) weights {
 }
 
 func makeMaxWeights(size int) weights {
-	weights := make(weights, 0)
+	weights := make(weights, 0, size)
 	for row := 0; row < size; row++ {
-		weights = append(weights, []float64{})
+		weights = append(weights, make([]float64, 0, size))
 		for column := 0; column < size; column++ {
 			weights[row] = append(weights[row], maxWeight)
 		}
@@ -77,8 +109,8 @@ func updateWeights(items []string, weights weights, marker rune,
 	prev := rune(0)
 	for row, item := range items {
 		weight := 0.0
+		item = strings.ToUpper(strings.ReplaceAll(item, "&&", "\a\a"))
 		for column, c := range item {
-			c = unicode.ToUpper(c)
 			i := slices.Index(chars, c)
 			if i > -1 { // c in alphabet
 				if column == 0 { // first
@@ -90,6 +122,7 @@ func updateWeights(items []string, weights weights, marker rune,
 				} else { // anywhere
 					weight = maxWeight - 1.0
 				}
+				weight += (float64(column) / 100.0) // mildly prefer earlier
 				if weights[row][i] > weight {
 					weights[row][i] = weight
 				}
@@ -99,42 +132,41 @@ func updateWeights(items []string, weights weights, marker rune,
 	}
 }
 
-func updateItems(items []string, marker byte, chars []rune,
-	indexes []int) int {
-	done := 0
+func applyIndexes(items []string, marker byte, chars []rune,
+	indexes []int) []string {
+	lines := make([]string, 0, len(items))
 	m := string(marker)
+	mm := m + m
 	for row, column := range indexes {
-		if column == -1 {
-			continue // unassigned
+		if row == len(items) {
+			break
+		}
+		line := items[row]
+		if column == -1 || len(line) == 0 {
+			lines = append(lines, line)
+			continue // unassigned or empty
+		}
+		uline := strings.ReplaceAll(strings.ToUpper(line), mm, "||")
+		i := strings.IndexByte(uline, marker)
+		if i > -1 {
+			lines = append(lines, line)
+			continue // user preset
 		}
 		c := chars[column]
-		if row < len(items) {
-			item := items[row]
-			i := strings.IndexByte(item, marker)
-			if i > -1 {
-				done++
-				continue // user preset
+		sc := string(c)
+		var index int                      // first is best
+		if !strings.HasPrefix(uline, sc) { // !first
+			index = strings.Index(uline, " "+sc)
+			if index > -1 { // start of word is second best
+				index++ // skip the space
+			} else { // anywhere is third best
+				index = strings.IndexRune(uline, c)
 			}
-			uitem := strings.ToUpper(item)
-			if len(uitem) == 0 {
-				continue // skip
-			}
-			var index int // first is best
-			sc := string(c)
-			if !strings.HasPrefix(uitem, sc) { // !first
-				index = strings.Index(uitem, " "+sc)
-				if index > -1 { // start of word is second best
-					index++ // skip the space
-				} else { // anywhere is third best
-					index = strings.IndexRune(uitem, c)
-				}
-			}
-			if index > -1 {
-				items[row] = item[:index] + m + item[index:]
-				done++
-			}
-
 		}
+		if index > -1 {
+			line = line[:index] + m + line[index:]
+		}
+		lines = append(lines, line)
 	}
-	return done
+	return lines
 }
--- accelhints_test.go
+++ accelhints_test.go
@@ -2,20 +2,18 @@ package accelhints
 
 import (
 	"testing"
-
-	"golang.org/x/exp/slices"
 )
 
 func Test001(t *testing.T) {
-	original := []string{"O_ne", "_Two"}
+	original := []string{"O&ne", "Two"}
 	expected := []string{"O&ne", "&Two"}
-	actual := slices.Clone(original)
-	n, err := AddHints(actual)
+	actual, err := Hints(original)
 	if err != nil {
 		t.Errorf("unexpected error: %s", err)
 	}
-	if n != len(original) {
-		t.Errorf("expected %d accelrated got %d", n, len(original))
+	_, n := Indexes(actual)
+	if n != 2 {
+		t.Errorf("expected 2 accelrated got %d", n)
 	}
 	for i := 0; i < len(original); i++ {
 		if actual[i] != expected[i] {
@@ -42,17 +40,57 @@ func Test002(t *testing.T) {
 		"&Paste",
 		"&Find",
 		"Find &Again"}
-	actual := slices.Clone(original)
-	n, err := AddHints(actual)
+	actual, err := Hints(original)
 	if err != nil {
 		t.Errorf("unexpected error: %s", err)
 	}
-	if n != len(original) {
-		t.Errorf("expected %d accelrated got %d", n, len(original))
+	_, n := Indexes(actual)
+	if n != 7 {
+		t.Errorf("expected 7 accelrated got %d", n)
 	}
 	for i := 0; i < len(original); i++ {
 		if actual[i] != expected[i] {
 			t.Errorf("expected %q, got %q", expected[i], actual[i])
+		}
+	}
+}
+
+func Test003(t *testing.T) {
+	original := []string{
+		"Undo",
+		"Redo",
+		"Copy",
+		"Cu&t",
+		"Paste",
+		"Find",
+		"Find Again",
+		"Find && Replace"}
+	expected := []string{
+		"&Undo",
+		"&Redo",
+		"&Copy",
+		"Cu&t",
+		"&Paste",
+		"&Find",
+		"Find &Again",
+		"F&ind && Replace"}
+	expectedIndexes := []int{0, 0, 0, 2, 0, 0, 5, 1}
+	actual, err := Hints(original)
+	if err != nil {
+		t.Errorf("unexpected error: %s", err)
+	}
+	indexes, n := Indexes(actual)
+	if n != 8 {
+		t.Errorf("expected 8 accelrated got %d", n)
+	}
+	for i := 0; i < len(original); i++ {
+		if actual[i] != expected[i] {
+			t.Errorf("expected %q, got %q", expected[i], actual[i])
+		}
+	}
+	for i := 0; i < len(original); i++ {
+		if indexes[i] != expectedIndexes[i] {
+			t.Errorf("expected %d, got %d", expectedIndexes[i], indexes[i])
 		}
 	}
 }
